1e82e113e9b083a8ca1917a95c27328199188245
Daniil <coolswood@yandex.ru>
2020-08-31 19:36:03 +0300

    update types

--- index.ts
+++ index.ts
@@ -1,3 +1,57 @@
-export type test = {
-    hi: string;
+declare namespace api {
+    namespace automaticMinds {
+        type diaryValue = {
+            conclusion: string;
+            dt: string | null;
+            errorsSelected: api.icons.logicErrors[] | [];
+            event: string;
+            id: number;
+            logicError: string;
+            moodRange: string;
+            moodSelected: api.icons.mood[] | [];
+            solution: string;
+        };
+    }
+
+    namespace psychologyTests {
+        type bdiValue = {
+            date: string | null;
+            id: number;
+            result: number[];
+            score: number;
+        };
+
+        type hduValue = {
+            date: string | null;
+            id: number;
+            result: number[];
+            steps: number[];
+        };
+    }
+
+    namespace icons {
+        export type logicErrors =
+            | 'max'
+            | 'common'
+            | 'filter'
+            | 'discval'
+            | 'jump'
+            | 'moreMine'
+            | 'emo'
+            | 'maybe'
+            | 'label'
+            | 'response';
+
+        export type mood =
+            | 'anger'
+            | 'alarm'
+            | 'sorrow'
+            | 'fear'
+            | 'irritation'
+            | 'despair'
+            | 'shame'
+            | 'insult'
+            | 'guilt'
+            | 'loneliness';
+    }
 }
